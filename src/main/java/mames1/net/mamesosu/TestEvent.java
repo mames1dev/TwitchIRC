cda750aebd3b8df8b4dd763e8f2d0b3beb1ee327
mames1dev <kamakuraowner@outlook.jp>
2025-04-23 09:20:11 +0900

    test ok

--- src/main/java/mames1/net/mamesosu/TestEvent.java
+++ src/main/java/mames1/net/mamesosu/TestEvent.java
@@ -7,5 +7,7 @@ public class TestEvent extends ListenerAdapter {
     @Override
     public void onMessage(org.pircbotx.hooks.events.MessageEvent event) {
         System.out.println(event.getUser().getNick() + ": " + event.getMessage());
+        // チャンネルへ参加
+        event.getBot().send().joinChannel("#mames1osu");
     }
 }
